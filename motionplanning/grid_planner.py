05de59486121b6c43ae4414b8e1cbc0dbb674003
Tung Phan <tung@caltech.edu>
2020-02-10 22:07:49 -0800

    add tube calc

--- motionplanning/grid_planner.py
+++ motionplanning/grid_planner.py
@@ -49,12 +49,61 @@ def uniform_sample_grid_points(np_bitmap, anchor, grid_size):
     return np.array(sample_points)
 
 #img_to_csv_bitmap(cv2.imread('imglib/AVP_planning_300p.png'), 'bitmap', verbose=True)
-np_bitmap = csv_bitmap_to_numpy_bitmap('bitmap')
-anchor = (1, 5)
-sampled = uniform_sample_grid_points(np_bitmap, anchor = [32, 22], grid_size = 10)
-plt.plot(sampled[:,0], sampled[:,1], '.')
-plt.show()
 
+def get_ball_neighbors(center, r):
+    r = int(np.ceil(r)) # robustify r
+    neighbors = []
+    dy_max = r
+    for dy in range(-dy_max, dy_max+1):
+        dx_max = int(np.floor(np.sqrt(r**2-dy**2)))
+        for dx in range(-dx_max, dx_max+1):
+            neighbors.append([center[0]+dx, center[1]+dy])
+    return np.unique(np.array(neighbors), axis=0)
+
+def get_rotation_matrix(theta):
+    return np.array([[np.cos(theta), -np.sin(theta)], [np.sin(theta), np.cos(theta)]])
+
+def rotate_vector(vec, theta):
+    rot_mat = get_rotation_matrix(theta)
+    return np.array([int(round(x)) for x in np.matmul(rot_mat, vec)])
+
+def get_rect_for_line(point1, point2, r):
+    angle = np.arctan2(point2[1] - point1[1], point2[0] - point1[0])
+    r = int(np.ceil(r)) # robustify r
+    d = int(np.ceil(np.sqrt((point1[0] - point2[0])**2 + (point1[1] - point2[1])**2)))
+    pre_displacements = []
+    for dy in range(-r, r+1):
+        for dx in range(0, d+1):
+            pre_displacements.append([dx, dy])
+    pre_displacements = np.matmul(np.array(pre_displacements), get_rotation_matrix(angle).transpose()).astype(int)
+    offset = np.tile(np.array(point1), (pre_displacements.shape[0], 1))
+    displacements = pre_displacements + offset
+    return np.unique(displacements, axis=0)
+
+def get_tube_for_line(point1, point2, r):
+    ball1 = get_ball_neighbors(point1, r)
+    ball2 = get_ball_neighbors(point2, r)
+    rect = get_rect_for_line(point1, point2, r)
+    tube = np.vstack((ball1, ball2, rect))
+    return np.unique(tube, axis=0)
+
+np_bitmap = csv_bitmap_to_numpy_bitmap('bitmap')
+anchor = [32, 55]
+grid_size = 10
+sampled = uniform_sample_grid_points(np_bitmap, anchor = anchor, grid_size = grid_size)
+
+point1 = [30, 20]
+point2 = [55, 80]
+point3 = [100, 100]
+r = 10
+tube1 = get_tube_for_line(point1, point2, r)
+tube2 = get_tube_for_line(point2, point3, r)
+
+plt.plot(sampled[:,0], sampled[:,1], '.')
+plt.plot(tube1[:,0], tube1[:,1])
+plt.plot(tube2[:,0], tube2[:,1])
+plt.axis('equal')
+plt.show()
 def to_planning_graph():
     pass
 
@@ -62,7 +111,6 @@ def make_grid(m, n):
     for i in range(m):
         for j in range(n):
             print(i,j)
-
 
 def A_star(start, end, weighted_graph):
     def get_manhattan_distance(start, end):
