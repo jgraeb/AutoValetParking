c5c2d4c66b7fbf4235683f2ac4008eb40e966b0b
jgraeb <jgraeben@caltech.edu>
2020-03-21 15:21:13 -0700

    update helper functions

---
+++ prepare/communication.py
@@ -0,0 +1,32 @@
+import trio
+import numpy as np
+
+def get_current_time(start_time):
+    return trio.current_time() - start_time
+
+def create_unidirectional_channel(sender, receiver, max_buffer_size, name = False):
+    out_channel, in_channel = trio.open_memory_channel(max_buffer_size)
+    if name:
+        sender.out_channels[name] = out_channel
+        receiver.in_channels[name] = in_channel
+    else:
+        sender.out_channels[receiver.name] = out_channel
+        receiver.in_channels[sender.name] = in_channel
+
+def create_bidirectional_channel(compA, compB, max_buffer_size):
+    create_unidirectional_channel(sender=compA, receiver=compB, max_buffer_size=max_buffer_size)
+    create_unidirectional_channel(sender=compB, receiver=compA, max_buffer_size=max_buffer_size)
+
+def set_up_channels(supervisor,planner, game, map_sys, customer, simulation):
+        create_bidirectional_channel(supervisor,planner,max_buffer_size=np.inf)
+        create_bidirectional_channel(customer, supervisor, max_buffer_size=np.inf)
+        create_unidirectional_channel(sender=customer, receiver=supervisor, max_buffer_size=np.inf, name='Request')
+        create_unidirectional_channel(sender=game, receiver=map_sys, max_buffer_size=np.inf, name='Enter')
+        create_unidirectional_channel(sender=game, receiver=map_sys, max_buffer_size=np.inf, name='Exit')
+        #create_unidirectional_channel(sender=game, receiver=map_sys, max_buffer_size=np.inf, name='PedEnter')
+        create_bidirectional_channel(map_sys, planner, max_buffer_size=np.inf)
+        create_unidirectional_channel(sender=supervisor, receiver=game, max_buffer_size=np.inf, name='GameEnter')
+        create_unidirectional_channel(sender=supervisor, receiver=game, max_buffer_size=np.inf, name='GameExit')
+        create_unidirectional_channel(sender=supervisor, receiver=game, max_buffer_size=np.inf, name='GameEnterPeds')
+        create_unidirectional_channel(sender=game, receiver=simulation, max_buffer_size=np.inf)
+        create_unidirectional_channel(sender=game, receiver=simulation, max_buffer_size=np.inf, name='PedSimulation')
--- variables/global_vars.py
+++ variables/global_vars.py
@@ -1,3 +1,4 @@
+import numpy as np
 import sys
 sys.path.append('..')
 current_time = 0
@@ -6,3 +7,21 @@ current_time = 0
 pedestrians_to_keep= set()
 cars_to_show = []
 pedestrians_to_show = []
+
+###
+average_arrival_rate = 0.5 # per second
+beta = 1/average_arrival_rate
+average_park_time = 200 # seconds
+MAX_BUFFER_SIZE = np.inf
+MAX_NO_PARKING_SPOTS = 1
+OPEN_TIME = 500 # not yet working
+ped_startpos = (2908,665)
+ped_endpos = (3160,665)
+TARGET_SPEED = 10/3.6 # 10 km/h
+SCALE_FACTOR_SIM = 3173/90 # scale from meters to pixels in simulation
+SCALE_FACTOR_PLAN = 90/300 # scale from pixels to meters in grid planner
+START_X = 120*SCALE_FACTOR_PLAN # m
+START_Y = 60*SCALE_FACTOR_PLAN # m
+START_YAW = 0 # rad
+start_walk_lane = (1287,665)
+end_walk_lane = (3160,665)
