6fab59d0ba47846d0a3afcaaf43190e2a92bf618
Tung Phan <tung@caltech.edu>
2020-06-19 01:13:40 -0700

    add curvature analysis

--- motionplanning/curvature_analysis.py
+++ motionplanning/curvature_analysis.py
@@ -3,6 +3,7 @@
 # Analysis for Motion Planning of Mobile Robots
 # https://users.wpi.edu/~rvcowlagi/publications/2014-tro-cbta.pdf
 
+import matplotlib.pyplot as plt
 from ipdb import set_trace as st
 import numpy as np
 
@@ -15,16 +16,18 @@ def solve_Acos_Bsin_C(A, B, C):
     c = C**2 - A**2
     return [np.arcsin(sol) for sol in np.roots([a, b, c])]
 
-if __name__ == '__main__':
-    # Example 1
-    d = 10 # grid size
-    y = 0 #
-    z = 5
-    w = 5
-    r = 45 # maximum radius of curvature (assuming larger than box for now)
-    beta_lo = -40/180*np.pi
-    beta_hi = 10/180*np.pi
-    a_star_w = np.arccos(1 - (d-w)/r)
+def get_Lambda_prime_x(spec):
+    new_spec = spec.get_altered_spec_for_lambda_prime_x_s1()
+    Xs, Us, _ = get_Upsilon_prime_x(new_spec)
+    Ls = [-u for u in Us]
+    return Xs, Ls
+
+def get_Upsilon_prime_x(spec):
+    d, y, z, w, r, beta_lo, beta_hi = spec.extract_params()
+    UPS = []
+    gammas = []
+    Xs = []
+
 
     if 2*r*(d-w) < d**2 + (d-w)**2:
         n2 = w + np.sqrt(r**2 - (r*np.sin(a_star_w)-d)**2) - r * np.cos(a_star_w)
@@ -45,13 +48,60 @@ if __name__ == '__main__':
                 B = sigma2
                 C = 1-(x-w)*np.cos(beta_lo)+d*np.sin(beta_lo)
                 Upsilon_prime_x = np.array(solve_Acos_Bsin_C(A, B, C))
-                print(Upsilon_prime_x / np.pi * 180)
+                max_val = np.min(Upsilon_prime_x / np.pi * 180)
+                if not np.iscomplex(max_val):
+                    UPS.append(max_val)
+                    Xs.append(x)
+                    break
             elif beta_lo <= gamma_star_x and gamma_star_x < beta_hi:
                 A = -(x-w)
                 B = d
                 C = sigma3
                 Upsilon_prime_x = np.array(solve_Acos_Bsin_C(A, B, C))
-                print(Upsilon_prime_x / np.pi * 180)
+                max_val = np.min(Upsilon_prime_x / np.pi * 180)
+                if not np.iscomplex(max_val):
+                    UPS.append(max_val)
+                    gammas.append(gamma_star_x / np.pi * 180)
+                    Xs.append(x)
+                    break
             else:
                 print('does not exist')
+    return Xs, UPS, gammas
+
+class Spec:
+    def __init__(self, d, y, z, w, r, beta_lo, beta_hi):
+        self.d = d
+        self.y = y
+        self.z = z
+        self.w = w
+        self.r = r
+        self.beta_lo = beta_lo
+        self.beta_hi = beta_hi
+    def extract_params(self):
+        return self.d, self.y, self.z, self.w, self.r, self.beta_lo, self.beta_hi
+
+    def get_altered_spec_for_lambda_prime_x_s1(self):
+        w = self.d -self.w
+        beta_lo = -self.beta_hi * (self.d - self.x)
+        beta_hi = -self.beta_lo * (self.d - self.x)
+        return Spec(d=self.d, y=self.y, z=self.z, w=w, r=self.r,
+                beta_lo=beta_lo, beta_hi=beta_hi)
+
+if __name__ == '__main__':
+    # Example 1
+    d = 10 # grid size
+    y = 0 #
+    z = 5
+    w = 1.25
+    r = 45 # maximum radius of curvature (assuming larger than box for now)
+    beta_lo = -40/180*np.pi
+    beta_hi = 10/180*np.pi
+    spec = Spec(d=d,y=y,z=z,w=w,r=r,beta_lo=beta_lo,beta_hi=beta_hi)
+    Xs, UPS, gammas = get_Upsilon_prime_x(spec)
+    Xs, LS = get_Lambda_prime_x(spec)
+    plt.plot(Xs, gammas, 'b')
+    plt.plot(Xs, UPS, 'k')
+    plt.plot(Xs, Ls, 'r')
+    plt.show()
+
 
