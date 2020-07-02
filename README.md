431d428c071844460565120359fe89e64d862e12
Tom <tomandersson94@gmail.com>
2020-07-02 12:03:32 +0200

    Implemented the functionality to read robot states (pos, orientation, velocity). Put this functionality and the setting of velocities into one common class, RobotCtrl. Updated the README.

--- README.md
+++ README.md
@@ -9,11 +9,11 @@ An example of modular contract-based design using a directive/response architect
 This branch introduces gazebo simulations of turtlebot3 for the automated valet parking. The functionallity and this README are currently under construction.
 To run this branch you need to have the AutomatedValetParking package in a catkin workspace for ROS melodic.
 
-### How to run a test for setting velocities to turtlebots in an empty gazebo world:
+### How to run a test for setting velocities and reading states of turtlebots in an empty gazebo world:
 1. Open a new terminal and go into your catkin workspace and source the generated setup file with the command: **source devel/setup.bash**
 2. Launch the turtlebots in an empty world with the command: **roslaunch gazebo main.launch**
-3. Open an new terminal and go to this folder AutoValetParking/gazebo/src. To begin publishing velocity topics to the turtlebots run the program robot_commander.py using the command: **python3 robot_commander.py**.
-4. Open an new terminal and go to this folder again AutoValetParking/gazebo/src. To set new velocities to the turtlebots run the program test_publisher.py using the command: **python3 test_publisher.py**.
+3. Open an new terminal and go to this folder: AutoValetParking/gazebo/src. To begin publishing velocity topics to the turtlebots and subscribing to their odometry topics, run the program robot_communication.py using the command: **python3 robot_communication.py**.
+4. Open an new terminal and go to this folder again: AutoValetParking/gazebo/src. To set new velocities to the turtlebots and read their states (position, orientation and velocity) run the program test_robot_ctrl.py using the command: **python3 test_robot_ctrl.py**.
 5. **[Optional]** To see the ROS structure open a new terminal and run the command: **rosrun rqt_graph rqt_graph**.
 6. **[Optional]** To change the number of turtlebots in the simulation change the variable nbr_of_robots in variables/global_vars.py and add more robots in the launch file gazebo/launch/robots.launch by copying the code for one robot and change the robot number to 2, 3 and so on.
 
---
+++ gazebo/src/RobotCtrl.py
@@ -0,0 +1,83 @@
+# Automated Valet Parking - Robot Control
+# Tom Andersson
+# California Institute of Technology
+# June, 2020
+
+import rospy
+import sys
+from std_msgs.msg import UInt16
+from std_msgs.msg import Float32MultiArray
+
+sys.path.append('../../') # enable importing modules from an upper directory:
+from variables.global_vars import nbr_of_robots
+
+class RobotCtrl:
+    """This class is used for setting velocities and reading the states of the robots in the gazebo simulation.
+    This is done through subscribed and published ros topics between this class and the robot_communication program.
+    Topics are only published and received opon request."""
+    
+    def __init__(self):
+        "Initializes the ros node, sets up subscribed and published topics and creates variables for storing the data."
+        rospy.init_node('robot_ctrl')        
+        self.pub_vel = rospy.Publisher('robot_set_vel', Float32MultiArray, queue_size=1)
+        self.pub_state_request = rospy.Publisher('robot_state_request', UInt16, queue_size=1)
+        rospy.Subscriber('robot_state', Float32MultiArray)
+
+        self.vel_commands = [[0, 0] for i in range(nbr_of_robots)]
+        self.robot_states = [[0, 0, 0, 0, 0] for i in range(nbr_of_robots)] #[pos_x, pos_y, orientation, lin_vel, rot_vel]
+        self.vel_topic = Float32MultiArray()
+
+
+    def set_lin_vel(self, robot_nbr, lin_vel):
+        "Sets a new linnear velocity for a given robot number. [m/s]"
+        if robot_nbr < 0 or robot_nbr >= nbr_of_robots:
+            raise Exception("Invalid robot number")
+        self.vel_commands[robot_nbr][0] = lin_vel
+        self.__publish_vel(robot_nbr)
+
+
+    def set_rot_vel(self, robot_nbr, rot_vel):
+        "Sets a new rotational velocity for a given robot number. [rad/s]"
+        if robot_nbr < 0 or robot_nbr >= nbr_of_robots:
+            raise Exception("Invalid robot number")
+        self.vel_commands[robot_nbr][1] = rot_vel
+        self.__publish_vel(robot_nbr)  
+
+
+    def set_vel(self, robot_nbr, lin_vel, rot_vel):
+        "Sets a new linear and rotational velocity for a given robot number. [m/s], [rad/s]"
+        if robot_nbr < 0 or robot_nbr >= nbr_of_robots:
+            raise Exception("Invalid robot number")
+        self.vel_commands[robot_nbr][0] = lin_vel
+        self.vel_commands[robot_nbr][1] = rot_vel
+        self.__publish_vel(robot_nbr)
+
+
+    def __publish_vel(self, robot_nbr):
+        "Publishes a new velocity for a given robot number. [m/s], [rad/s]"
+        if robot_nbr < 0 or robot_nbr >= nbr_of_robots:
+            raise Exception("Invalid robot number")
+        self.vel_topic.data = [float(robot_nbr), float(self.vel_commands[robot_nbr][0]), float(self.vel_commands[robot_nbr][1])]
+        self.pub_vel.publish(self.vel_topic)
+
+    
+    def __update_state(self, robot_nbr):
+        self.pub_state_request.publish(robot_nbr)
+        self.robot_states[robot_nbr] = rospy.wait_for_message('robot_state', Float32MultiArray).data
+
+    
+    def get_pos(self, robot_nbr):
+        """Returns the x, y position, [m], as well as the orientation around z-axis starting from positive x-axis,
+        e(-pi - pi) [rad], for a given robot number. (x_pos, y_pos, orientation)"""
+        if robot_nbr < 0 or robot_nbr >= nbr_of_robots:
+            raise Exception("Invalid robot number")
+        self.__update_state(robot_nbr)
+        return (self.robot_states[robot_nbr][0], self.robot_states[robot_nbr][1], self.robot_states[robot_nbr][2])
+
+
+    def get_vel(self, robot_nbr):
+        "Returns the linear and rorational velocity for a given robot number. (lin_vel, rot_vel) [m/s], [rad/s]"
+        if robot_nbr < 0 or robot_nbr >= nbr_of_robots:
+            raise Exception("Invalid robot number")
+        self.__update_state(robot_nbr)
+        return (self.robot_states[robot_nbr][3], self.robot_states[robot_nbr][4])
