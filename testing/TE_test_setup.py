064cef2c78149b8264f2364348336696358a9218
Apurva Badithela <13437374+abadithela@users.noreply.github.com>
2020-08-04 16:26:07 -0700

    Add proposition functions

--- testing/TE_test_setup.py
+++ testing/TE_test_setup.py
@@ -14,6 +14,7 @@ from dynamic_environment.gridworld_classes.gridworld_class import GridWorld
 from dynamic_environment.gridworld_classes.Player_class import Player
 from dynamic_environment.transitions_classes.General_Game_Graph_class import GeneralGameGraph
 from static_environment.transitions.test_run_configuration_transitions import test_run_configuration 
+from coverage_propositions.lamda_functions import construct_lambda_function
 
 # Setting up abstraction / transition systems for the AVP system and the test environment
 # Function to load System and Environment transition dictionaries from pkl files
@@ -31,6 +32,22 @@ def construct_test_run_config(Tsys, Tenv):
     return test_run_config
 
 # Setting up propositions to decide the test case
+# Return propositions you want to cover in a single test run
+# Format: sys_prop_list = [[2,3,4], [6,7,8]], env_prop_list = [[3,6,7], [8,9]], connectives = ["and", "or", "and"]
+# Ex: Lambda function returned for sys_prop_list[0] and env_prop_list[0] would be: lambda ns, ne: ns in sys_prop_lsit[0] and ne in env_prop_list[0]
+# If only system states are pertinent, leave the env_prop_list at the corresponding index empty
+def setup_propositions(sys_prop_list, env_prop_list, connectives_list):
+    Nprop = len(connectives_list)
+    assert(Nprop == len(sys_prop_list))
+    assert(len(sys_prop_list) == Nprop)
+    lambda_functions = []
+    for ii in range(Nprop):
+        func = construct_lambda_function(sys_prop_list[ii], env_prop_list[ii], connectives_list[ii])
+        if func != []:
+            lambda_functions.append(func)
+        else:
+            print("Err: Don't enter empty lists for system and environment propositions at the same time.")
+    return lambda_functions
 
 # Setting up Test Configuration with static obstacles
 
